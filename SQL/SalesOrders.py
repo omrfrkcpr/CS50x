aef7cf894ae122f2067e6453cf5e43350d62a587
omrfrkcpr <omerrfarukcapur@gmail.com>
2023-12-01 17:00:38 +0100

    updated

--- SQL/SalesOrders.py
+++ SQL/SalesOrders.py
@@ -2,6 +2,18 @@ import csv
 
 with open("SalesOrders.csv", "r") as file:
     reader = csv.DictReader(file)
+    counts = {}
+    # East, Central, West = 0, 0, 0
     for row in reader:
         favorite = row["Region"]
-        print(favorite)
+        counts[favorite] += 1
+        # if favorite == "Central":
+        #     Central += 1
+        # elif favorite == "East":
+        #     East += 1
+        # elif favorite == "West":
+        #     West += 1
+
+print(f"Central: {Central}")
+print(f"East: {East}")
+print(f"West: {Central}")
